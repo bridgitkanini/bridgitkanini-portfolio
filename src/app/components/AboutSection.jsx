76ffb3bb3b06deeb38675653613eade1bd7b25b7
bkm-code <bridgit.k.mutinda@gmail.com>
2024-03-13 15:49:29 +0300

    Update AboutSection.jsx

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -9,12 +9,12 @@ const TAB_DATA = [
     id: "skills",
     content: (
       <ul className="list-disc pl-2">
+        <li>JavaScript</li>
+        <li>React.js</li>
+        <li>Next.js</li>
+        <li>Vite</li>
         <li>Node.js</li>
         <li>Express</li>
-        <li>PostgreSQL</li>
-        <li>Sequelize</li>
-        <li>JavaScript</li>
-        <li>React</li>
       </ul>
     ),
   },
@@ -23,8 +23,11 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>Fullstack Academy of Code</li>
-        <li>University of California, Santa Cruz</li>
+        <li>freeCodeCamp.org</li>
+        <li>
+          Jomo Kenyatta University of Agriculture and Technology, Nairobi -
+          Kenya
+        </li>
       </ul>
     ),
   },
@@ -33,8 +36,10 @@ const TAB_DATA = [
     id: "certifications",
     content: (
       <ul className="list-disc pl-2">
-        <li>AWS Cloud Practitioner</li>
-        <li>Google Professional Cloud Developer</li>
+        <li>freeCodeCamp - Javascript Algorithms and Data Structures.</li>
+        <li>freeCodeCamp - Frontend Web Development Libraries.</li>
+        <li>freeCodeCamp - Data Visualisation with D3.</li>
+        <li>freeCodeCamp - Backend Web Development and APIs.</li>
       </ul>
     ),
   },
@@ -53,16 +58,20 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about-image.png" width={500} height={500} />
+        <Image src="/images/about-image-new.png" width={500} height={500} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-            I am a full stack web developer with a passion for creating
-            interactive and responsive web applications. I have experience
-            working with JavaScript, React, Redux, Node.js, Express, PostgreSQL,
-            Sequelize, HTML, CSS, and Git. I am a quick learner and I am always
-            looking to expand my knowledge and skill set. I am a team player and
-            I am excited to work with others to create amazing applications.
+            I am a self-taught programmer currently specializing in Frontend Web
+            Development. My expertise lies in JavaScript, ReactJS, HTML, and
+            CSS; Sass, Tailwind CSS. I am passionate about creative
+            problem-solving and have a keen eye for detail, always striving to
+            enhance the design of web applications. I am a quick learner and I
+            am always looking to expand my knowledge and skill set. Currently, I
+            am delving into NodeJS, full-stack web development, and advanced
+            computer science algorithms. I thrive in collaborative environments
+            and I'm adept at working within teams to achieve goals, meet
+            targets, and provide innovative technological solutions.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
