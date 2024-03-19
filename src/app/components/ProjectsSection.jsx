9ef79b90c491c689a767aa28b92f0ffe4dbb5896
bkm-code <bridgit.k.mutinda@gmail.com>
2024-03-20 01:08:16 +0300

    Update ProjectsSection.jsx

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -77,8 +77,8 @@ const ProjectsSection = () => {
 
   return (
     <section id="projects">
-      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
-        My Projects
+      <h2 className="text-center text-5xl font-bold text-white mt-4 mb-8 md:mb-12">
+        What I Have Done...
       </h2>
       <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
         <ProjectTag
