313deee8706165e6920692c3dfecfc5cbdcd43d3
bkm-code <bridgit.k.mutinda@gmail.com>
2024-04-26 01:58:43 +0300

    Remove div styling, added to individual components.

--- src/app/page.js
+++ src/app/page.js
@@ -9,16 +9,14 @@ import SkillsSection from "./components/SkillsSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#1f1e39]">
+    <main className="flex min-h-screen flex-col bg-[#1f1e39] py-4">
       <Navbar />
-      <div className="container mt-24 mx-auto px-12 py-4">
-        <HeroSection />
-        <AchievementsSection />
-        <AboutSection />
-        <SkillsSection />
-        <ProjectsSection />
-        <EmailSection />
-      </div>
+      <HeroSection />
+      <AchievementsSection />
+      <AboutSection />
+      <SkillsSection />
+      <ProjectsSection />
+      <EmailSection />
       <Footer />
     </main>
   );
