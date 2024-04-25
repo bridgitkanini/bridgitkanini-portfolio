7d84954125a1a0fa2f8b7aa3495309efe30c6e6c
bkm-code <bridgit.k.mutinda@gmail.com>
2024-04-26 01:54:40 +0300

    Adjust padding and margin.

--- src/app/components/AchievementsSection.jsx
+++ src/app/components/AchievementsSection.jsx
@@ -29,8 +29,8 @@ const achievementsList = [
 
 const AchievementsSection = () => {
   return (
-    <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-      <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-between">
+    <div className="xl:gap-16 sm:py-16 xl:px-16 -mt-14">
+      <div className="sm:border-[#33353F] sm:border rounded-md px-16 flex flex-col sm:flex-row items-center justify-between">
         {achievementsList.map((achievement, index) => {
           return (
             <div
