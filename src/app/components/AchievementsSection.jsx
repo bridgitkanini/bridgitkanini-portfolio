2fd22cbb7d81a13ca3ffa66a6ebcd1aab3bd1aff
bkm-code <bridgit.k.mutinda@gmail.com>
2024-05-23 16:57:43 +0300

    Achievements component redesign, remove numbers animation.

--- src/app/components/AchievementsSection.jsx
+++ src/app/components/AchievementsSection.jsx
@@ -1,63 +1,85 @@
 "use client";
 import React from "react";
 import dynamic from "next/dynamic";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFolderOpen,
+  faUserGroup,
+  faCertificate,
+} from "@fortawesome/free-solid-svg-icons";
 
-const AnimatedNumbers = dynamic(
-  () => {
-    return import("react-animated-numbers");
-  },
-  { ssr: false }
-);
+const AnimatedNumbers = dynamic(() => import("react-animated-numbers"), {
+  ssr: false,
+});
 
 const achievementsList = [
   {
-    metric: "Projects",
+    icon: faFolderOpen,
+    metric: "Projects Completed",
     value: "20",
     postfix: "+",
   },
   {
+    icon: faUserGroup,
     prefix: "~",
     metric: "Users",
     value: "1000",
   },
   {
-    metric: "Months",
-    value: "4",
+    icon: faCertificate,
+    metric: "Year Experience",
+    value: "1",
     postfix: "+",
   },
 ];
 
 const AchievementsSection = () => {
   return (
-    <div className="xl:gap-16 sm:py-16 xl:px-16 -mt-14">
-      <div className="sm:border-[#33353F] sm:border rounded-md px-16 flex flex-col sm:flex-row items-center justify-between">
-        {achievementsList.map((achievement, index) => {
-          return (
-            <div
-              key={index}
-              className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
-            >
-              <h2 className="text-teal-500 text-3xl font-bold flex flex-row">
-                {achievement.prefix}
-                <AnimatedNumbers
-                  includeComma
-                  animateToNumber={parseInt(achievement.value)}
-                  locale="en-US"
-                  className="text-white text-4xl font-bold"
-                  configs={(_, index) => {
-                    return {
+    <div className="xl:gap-16 sm:py-8">
+      <div className="rounded-md flex flex-col sm:flex-row items-center gap-8">
+        {achievementsList.map((achievement, index) => (
+          <div
+            key={index}
+            className="flex flex-col items-center justify-center my-4 sm:my-0 bg-[#181919] py-10 px-2 rounded-[30%]"
+          >
+            <div className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0">
+              <FontAwesomeIcon
+                icon={achievement.icon}
+                className="text-slate-200 mb-2"
+              />
+              <h2 className="text-3xl font-bold flex flex-row items-center">
+                {achievement.prefix && (
+                  <span className="mr-1 text-transparent bg-clip-text bg-gradient-to-b from-amber-500 from-50% to-[#181919]">
+                    {achievement.prefix}
+                  </span>
+                )}
+                {/* <div className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-amber-100 from-25% via-amber-500 via-50% to-amber-900 to-70%">
+                  <AnimatedNumbers
+                    includeComma
+                    animateToNumber={parseInt(achievement.value)}
+                    locale="en-US"
+                    className="text-4xl font-bold"
+                    configs={(_, index) => ({
                       mass: 1,
                       friction: 100,
-                      tensions: 140 * (index + 1),
-                    };
-                  }}
-                />
-                {achievement.postfix}
+                      tension: 140 * (index + 1),
+                    })}
+                  />
+                </div> */}
+                <div className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-b from-amber-500 from-50% to-[#181919]">
+                  {achievement.value}
+                </div>
+
+                {achievement.postfix && (
+                  <span className="ml-1 text-transparent bg-clip-text bg-gradient-to-b from-amber-500 from-50% to-[#181919]">
+                    {achievement.postfix}
+                  </span>
+                )}
               </h2>
-              <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
+              <p className="text-white text-xs mt-1">{achievement.metric}</p>
             </div>
-          );
-        })}
+          </div>
+        ))}
       </div>
     </div>
   );
