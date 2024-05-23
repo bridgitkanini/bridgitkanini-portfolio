7d00e01aab84463d64031a82be1b76c737682d83
bkm-code <bridgit.k.mutinda@gmail.com>
2024-05-23 16:59:12 +0300

    About page redesign.

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -2,13 +2,15 @@
 import React, { useTransition, useState } from "react";
 import Image from "next/image";
 import TabButton from "./TabButton";
+import AchievementsSection from "./AchievementsSection";
+import Link from "next/link";
 
 const TAB_DATA = [
   {
     title: "Skills",
     id: "skills",
     content: (
-      <ul className="list-disc pl-2">
+      <ul className="list-disc grid grid-cols-2 gap-x-1 pl-2 text-sm">
         <li>JavaScript</li>
         <li>React.js</li>
         <li>Next.js</li>
@@ -22,7 +24,7 @@ const TAB_DATA = [
     title: "Education",
     id: "education",
     content: (
-      <ul className="list-disc pl-2">
+      <ul className="list-disc pl-2 text-sm">
         <li>freeCodeCamp.org</li>
         <li>
           Jomo Kenyatta University of Agriculture and Technology, Nairobi -
@@ -35,12 +37,12 @@ const TAB_DATA = [
     title: "Certifications",
     id: "certifications",
     content: (
-      <ul className="list-disc pl-2">
+      <ul className="list-disc pl-2 text-sm">
         <li>
           <a
             href="https://www.freecodecamp.org/certification/bkm-code/javascript-algorithms-and-data-structures"
             target="_blank"
-            className="hover:text-teal-300"
+            className="hover:text-amber-300"
           >
             freeCodeCamp - Javascript Algorithms and Data Structures.
           </a>
@@ -49,7 +51,7 @@ const TAB_DATA = [
           <a
             href="https://www.freecodecamp.org/certification/bkm-code/front-end-development-libraries"
             target="_blank"
-            className="hover:text-teal-300"
+            className="hover:text-amber-300"
           >
             freeCodeCamp - Frontend Web Development Libraries.
           </a>
@@ -58,7 +60,7 @@ const TAB_DATA = [
           <a
             href="https://www.freecodecamp.org/certification/bkm-code/data-visualization"
             target="_blank"
-            className="hover:text-teal-300"
+            className="hover:text-amber-300"
           >
             freeCodeCamp - Data Visualisation with D3.
           </a>
@@ -67,7 +69,7 @@ const TAB_DATA = [
           <a
             href="https://www.freecodecamp.org/certification/bkm-code/responsive-web-design"
             target="_blank"
-            className="hover:text-teal-300"
+            className="hover:text-amber-300"
           >
             freeCodeCamp - Backend Web Development and APIs.
           </a>
@@ -88,20 +90,21 @@ const AboutSection = () => {
   };
 
   return (
-    <section className="text-[#1f1e39] py-24 bg-[#e0e1e1] mt-24 mx-auto px-12" id="about">
-      <h2 className="text-center text-5xl font-bold text-[#1f1e39]">
-        More About Me...
+    <section id="about" className="py-4 mx-auto px-8">
+      <h2 className="text-center text-transparent mt-8 bg-clip-text bg-gradient-to-r from-amber-100 from-25% via-amber-500 via-50% to-amber-700 to-70% text-2xl sm:text-2xl lg:text-5xl lg:leading-normal font-black">
+        About Me...
       </h2>
-      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+      <div className="md:grid md:grid-cols-2 gap-4 px-4 xl:gap-4 sm:py-4 xl:px-4">
         <Image
           src="/images/about-image02.png"
           alt="About Image"
-          width={500}
-          height={500}
-          className="-mt-8"
+          width={400}
+          height={400}
+          className="rounded-full mt-20 ml-10"
         />
-        <div className="md:mt-0 text-left flex flex-col h-full">
-          <p className="text-base lg:text-lg py-8">
+        <div className="realtive md:mt-0 text-left flex flex-col h-full -ml-10 pr-10">
+          <AchievementsSection />
+          <p className="text-md lg:text-md ">
             I write JavaScript, ReactJS, HTML, and CSS; Sass, Tailwind CSS. I am
             passionate about creative problem-solving and have a keen eye for
             detail, always striving to enhance the design of web applications. I
@@ -131,10 +134,20 @@ const AboutSection = () => {
               Certifications{" "}
             </TabButton>
           </div>
-          <div className="mt-8">
+          <div className="mt-2">
             {TAB_DATA.find((t) => t.id === tab).content}
           </div>
         </div>
+      </div>
+      <div className="ml-80 -mt-8">
+        <Link
+          href="/#contact"
+          className="absolute ml-72 mt-1 px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-amber-200 to-amber-600 hover:bg-amber-500 text-amber-400"
+        >
+          <span className="block bg-[#241c0a] hover:tracking-widest transition-all ease-in-out duration-1000 rounded-full px-4 py-1 -mx-0.5 -my-0.5">
+            Request resume
+          </span>
+        </Link>
       </div>
     </section>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -4,17 +4,19 @@ import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
-import AchievementsSection from "./components/AchievementsSection";
 import SkillsSection from "./components/SkillsSection";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#0d0c0d] py-4">
       <Navbar />
       <HeroSection />
-      {/* <AchievementsSection /> */}
       <AboutSection />
-      <SkillsSection />
+      {/* <SkillsSection /> */}
       <ProjectsSection />
       <EmailSection />
       <Footer />
