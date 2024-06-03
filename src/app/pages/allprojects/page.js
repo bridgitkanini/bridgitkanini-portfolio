9ea449a5ecbf59d875a442c7c93cf0b895ce3699
bkm-code <bridgit.k.mutinda@gmail.com>
2024-06-04 00:32:18 +0300

    All projects page

---
+++ src/app/pages/allprojects/page.js
@@ -0,0 +1,10 @@
+import Layout from "@/app/components/Layout";
+import AllProjects from "@/app/components/AllProjects";
+
+export default function AllProjectsPage() {
+  return (
+    <Layout>
+      <AllProjects />
+    </Layout>
+  );
+}
