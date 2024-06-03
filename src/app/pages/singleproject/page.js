7fadedd5dfe3528e27aec78cb1a21e18b8f07c39
bkm-code <bridgit.k.mutinda@gmail.com>
2024-06-04 00:32:53 +0300

    Single projects page.js

---
+++ src/app/pages/singleproject/page.js
@@ -0,0 +1,11 @@
+import Layout from "@/app/components/Layout"
+import SingleProject from "@/app/components/SingleProject";
+
+
+export default function SingleProjectsPage() {
+  return (
+    <Layout>
+      <SingleProject />
+    </Layout>
+  );
+}
