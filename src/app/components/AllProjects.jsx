9db6190b01e705f0258030bb30e836827bf67379
bkm-code <bridgit.k.mutinda@gmail.com>
2024-06-04 22:45:37 +0300

    Grid image layout in all projects page

--- src/app/components/AllProjects.jsx
+++ src/app/components/AllProjects.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const AllProjects = () => {
-  return <div id="allprojects">AllProjects</div>;
+  return (
+    <div id="allprojects">
+      <div>
+        <div>Image1</div>
+        <div>
+          <div>Image2</div>
+          <div>
+            <div>Image3</div>
+            <div>Image4</div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div>Image5</div>
+        <div>Image6</div>
+        <div>Image7</div>
+      </div>
+    </div>
+  );
 };
 
 export default AllProjects;
