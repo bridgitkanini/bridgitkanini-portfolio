0879643d73344b53f7740741f551e0092834885d
bkm-code <bridgit.k.mutinda@gmail.com>
2024-06-05 14:35:27 +0300

    Create grid layout gallery

--- src/app/components/AllProjects.jsx
+++ src/app/components/AllProjects.jsx
@@ -1,23 +1,19 @@
 import React from "react";
+import { projectsData } from "./ProjectsData.jsx";
 
 const AllProjects = () => {
+  const imgStyle = "flex items-center justify-center";
+
   return (
-    <div id="allprojects" className="my-40">
-      <div>
-        <div>Image1</div>
-        <div>
-          <div>Image2</div>
-          <div>
-            <div>Image3</div>
-            <div>Image4</div>
-          </div>
+    <div id="allprojects" className="my-40 grid md:grid-cols-5 gap-4">
+      {projectsData.map((project, i) => (
+        <div
+          key={i}
+          className={`${imgStyle} ${i === 0 || i === 5 ? "md:col-span-3" : ""}`}
+        >
+          <img src={project.image} alt={project.alt} className="rounded-xl" />
         </div>
-      </div>
-      <div>
-        <div>Image5</div>
-        <div>Image6</div>
-        <div>Image7</div>
-      </div>
+      ))}
     </div>
   );
 };
