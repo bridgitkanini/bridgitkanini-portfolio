cb5e0ecc2470eeab669d176f216df92168f6ef16
bkm-code <bridgit.k.mutinda@gmail.com>
2024-06-05 14:34:58 +0300

    Reorganise project data

--- src/app/components/ProjectsData.jsx
+++ src/app/components/ProjectsData.jsx
@@ -1,45 +1,12 @@
 export const projectsData = [
   {
     id: 1,
-    title: "React Portfolio Website",
-    description:
-      "A responsive and customizable portfolio website using NextJs and Tailwind",
-    image: "/images/projects/1.png",
-    alt: "Project 1 Image",
-    tag: ["All", "Web"],
-    gitUrl: "https://github.com/bridgitkanini/bridgitkanini-portfolio",
-    previewUrl: "/",
-    techStack: [
-      {
-        name: "React",
-        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
-      },
-      {
-        name: "NodeJS",
-        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
-      },
-      {
-        name: "MySQL",
-        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
-      },
-      {
-        name: "Sass",
-        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg",
-      },
-      {
-        name: "Javascript",
-        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
-      },
-    ],
-  },
-  {
-    id: 2,
     title: "AfriVazi",
     description:
       "Designed and implemented a shopping app dedicated to African-inspired fashion, featuring vibrant and culturally rich designs like Ankara, Asoebi, and Dashiki. The app offers a seamless user experience, integrating advanced search and filter options to help users find their perfect style.",
     description2: "",
-    image: "/images/projects/3.png",
-    alt: "Project 2 Image",
+    image: "/images/projects/AfriVazi.png",
+    alt: "Project 1 Image",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/bridgitkanini/AfriVazi",
     previewUrl: "https://afri-vazi.vercel.app/",
@@ -67,12 +34,12 @@ export const projectsData = [
     ],
   },
   {
-    id: 3,
+    id: 2,
     title: "CineVerse",
     description:
       "Created a dynamic movie reviews app that allows users to explore, rate, and review the latest films. The app features a user-friendly interface with personalized recommendations, advanced search capabilities, and community engagement tools.",
-    image: "/images/projects/CineVerse.png",
-    alt: "Project 3 Image",
+    image: "/images/projects/3.png",
+    alt: "Project 2 Image",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/bridgitkanini/CineVerse",
     previewUrl: "/",
@@ -100,15 +67,48 @@ export const projectsData = [
     ],
   },
   {
-    id: 4,
-    title: "Mental Health Quotes",
+    id: 3,
+    title: "QuoteTherapy",
     description:
       "An SPA generating fresh quotes and background colors with each 'Next' button press, enabling users to share uplifting messages on mental health across social media platforms.",
-    image: "/images/projects/Random-Quote-Machine.png",
-    alt: "Project 4 Image",
+    image: "/images/projects/6.png",
+    alt: "Project 3 Image",
     tag: ["All", "Mobile"],
     gitUrl: "https://github.com/bridgitkanini/fCC-front-end",
     previewUrl: "https://codepen.io/Bridgit-Kanini/pen/zYeQNWR",
+    techStack: [
+      {
+        name: "React",
+        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+      },
+      {
+        name: "NodeJS",
+        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+      },
+      {
+        name: "MySQL",
+        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Sass",
+        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg",
+      },
+      {
+        name: "Javascript",
+        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "TripQuest",
+    description:
+      "A simple app demonstrating CRUD operations in backend development.",
+    image: "/images/projects/4.png",
+    alt: "Project 4 Image",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/bridgitkanini/CRUD-API-Node-Express-Mongodb",
+    previewUrl: "/",
     techStack: [
       {
         name: "React",
@@ -165,4 +165,70 @@ export const projectsData = [
       },
     ],
   },
+  {
+    id: 6,
+    title: "TalentTrackr",
+    description:
+      "A simple app demonstrating CRUD operations in backend development.",
+    image: "/images/projects/2.png",
+    alt: "Project 6 Image",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/bridgitkanini/CRUD-API-Node-Express-Mongodb",
+    previewUrl: "/",
+    techStack: [
+      {
+        name: "React",
+        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+      },
+      {
+        name: "NodeJS",
+        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+      },
+      {
+        name: "MySQL",
+        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Sass",
+        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg",
+      },
+      {
+        name: "Javascript",
+        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+      },
+    ],
+  },
+  // {
+  //   id: 7,
+  //   title: "FinTechApp",
+  //   description:
+  //     "A simple app demonstrating CRUD operations in backend development.",
+  //   image: "/images/projects/1.png",
+  //   alt: "Project 7 Image",
+  //   tag: ["All", "Web"],
+  //   gitUrl: "https://github.com/bridgitkanini/CRUD-API-Node-Express-Mongodb",
+  //   previewUrl: "/",
+  //   techStack: [
+  //     {
+  //       name: "React",
+  //       icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+  //     },
+  //     {
+  //       name: "NodeJS",
+  //       icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+  //     },
+  //     {
+  //       name: "MySQL",
+  //       icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+  //     },
+  //     {
+  //       name: "Sass",
+  //       icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg",
+  //     },
+  //     {
+  //       name: "Javascript",
+  //       icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+  //     },
+  //   ],
+  // },
 ];
