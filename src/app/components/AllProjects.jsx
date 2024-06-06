0df30a7eddfbb78f62462cdbb022c568734c9324
bkm-code <bridgit.k.mutinda@gmail.com>
2024-06-06 14:18:35 +0300

    Layout attempt

--- src/app/components/AllProjects.jsx
+++ src/app/components/AllProjects.jsx
@@ -5,15 +5,39 @@ const AllProjects = () => {
   const imgStyle = "flex items-center justify-center";
 
   return (
-    <div id="allprojects" className="my-40 grid md:grid-cols-5 gap-4">
-      {projectsData.map((project, i) => (
-        <div
-          key={i}
-          className={`${imgStyle} ${i === 0 || i === 5 ? "md:col-span-3" : ""}`}
-        >
-          <img src={project.image} alt={project.alt} className="rounded-xl" />
-        </div>
-      ))}
+    <div id="allprojects" className="my-40">
+      <div className="grid md:grid-cols-6 md:grid-row-4 gap-4">
+        {projectsData.slice(0, 3).map((project, i) => (
+          <div
+            key={i}
+            className={`${imgStyle} ${
+              i === 0
+                ? "md:col-span-4 md:row-span-2"
+                : i === 1 || i === 2
+                ? "md:col-span-2"
+                : ""
+            }`}
+          >
+            <img src={project.image} alt={project.alt} className="rounded-xl" />
+          </div>
+        ))}
+      </div>
+      <div className="grid md:grid-cols-6 md:grid-row-4 gap-4">
+        {projectsData.slice(3).map((project, i) => (
+          <div
+            key={i}
+            className={`${imgStyle} ${
+              i === 2
+                ? "md:col-span-4 md:row-span-2"
+                : i === 1 || i === 0
+                ? "md:col-span-2"
+                : ""
+            }`}
+          >
+            <img src={project.image} alt={project.alt} className="rounded-xl" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
