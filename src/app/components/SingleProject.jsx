dd70621596fe8904b8f4e578e30a65c34c375a60
bkm-code <bridgit.k.mutinda@gmail.com>
2024-06-08 19:43:07 +0300

    Update

--- src/app/components/SingleProject.jsx
+++ src/app/components/SingleProject.jsx
@@ -7,9 +7,9 @@ const SingleProject = () => {
     <div id="singleproject" className="my-40">
       <h3>BKM-CODE PORTFOLIO</h3>
       <h1>
-        {/* Project {id} {title} */}
+        Project {id} {title}
       </h1>
-      {/* <ProjectCard
+      <ProjectCard
         key={project.id}
         title={project.title}
         description={project.description2}
@@ -17,7 +17,7 @@ const SingleProject = () => {
         gitUrl={project.gitUrl}
         previewUrl={project.previewUrl}
         techStack={project.techStack} // Pass tech stack data here
-      /> */}
+      />
       <div className="flex flex-row gap-4">
         <Link
           href="" 
@@ -25,7 +25,7 @@ const SingleProject = () => {
           className="mt-1 px-1 inline-block py-1 rounded-full bg-gradient-to-br from-amber-200 to-amber-600 hover:bg-amber-500 text-amber-400"
         >
           <span className="block bg-[#241c0a] hover:tracking-widest transition-all ease-in-out duration-1000 rounded-full px-4 py-1 -mx-0.5 -my-0.5">
-            {/* Go to {title} */}
+            Go to {title}
           </span>
         </Link>
         <Link
