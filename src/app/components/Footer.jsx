7e8087f2ccdaa985dfb2b6eb707004e506bf02d6
bkm-code <bridgit.k.mutinda@gmail.com>
2024-06-13 14:03:54 +0300

    Add margin between footer and send button on mobile.

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer fixed sm:relative bottom-0 w-full bg-[#0d0c0d] border z-10 border-t-[#181919] border-l-transparent border-r-transparent border-b-transparent">
-      <div className="container p-4 mt-4 flex justify-between">
+      <div className="container p-4 mt-8 flex justify-between">
         <p className="text-xs sm:text-base text-slate-400">© Copyright 2024</p>
         <span className="text-xs text-white font-normal">BKM-CODE</span>
         <p className="text-xs sm:text-base text-slate-400">All rights reserved.</p>
