d232a4072392a7371eaacecbddd3ef5b2645e1d4
bkm-code <bridgit.k.mutinda@gmail.com>
2024-06-13 14:05:14 +0300

    Add background color to secondary pages.

--- src/app/components/AllProjects.jsx
+++ src/app/components/AllProjects.jsx
@@ -9,49 +9,51 @@ const AllProjects = () => {
     "flex flex-col items-center justify-center hover:scale-105 duration-300";
 
   return (
-    <div id="allprojects" className="xl:gap-16 sm:py-16 xl:px-16 mx-2">
-      <div className="flex flex-col my-10 sm:mb-16">
-        <div className="flex items-center">
-          <h1 className="ml-2 flex-grow text-center text-transparent bg-clip-text bg-gradient-to-r from-amber-100 from-20% via-amber-300 via-40% to-amber-600 to-60% text-lg sm:text-2xl lg:leading-normal font-black">
-            BKM-CODE PORTFOLIO
-          </h1>
-        </div>
-      </div>
-      <div className="flex flex-col items-center justify-center my-4">
-        <span className="text-white text-xs mb-1">click to view</span>
-        <FontAwesomeIcon
-          icon={faAnglesDown}
-          className="text-amber-400 text-sm sm:text-2xl animate-bounce"
-        />
-      </div>
-      <div className="grid grid-cols-4 gap-4 h-full w-full">
-        {projectsData.map((project, i) => (
-          <div
-            key={i}
-            className={`${boxStyle} ${
-              i === 0 || i === 4 || i === 5 || i === 6
-                ? "col-span-2"
-                : i === 2
-                ? "row-span-2"
-                : ""
-            }`}
-          >
-            <Link href={`/singleproject/${project.id}`}>
-              <img
-                src={project.image}
-                alt={project.alt}
-                className="sm:h-full"
-              />
-            </Link>
+    <div className="bg-[#0d0c0d] h-full">
+      <div id="allprojects" className="xl:gap-16 sm:py-16 xl:px-16 mx-2">
+        <div className="flex flex-col pt-10 sm:mb-16">
+          <div className="flex items-center">
+            <h1 className="ml-2 flex-grow text-center text-transparent bg-clip-text bg-gradient-to-r from-amber-100 from-20% via-amber-300 via-40% to-amber-600 to-60% text-lg sm:text-2xl lg:leading-normal font-black">
+              BKM-CODE PORTFOLIO
+            </h1>
           </div>
-        ))}
+        </div>
+        <div className="flex flex-col items-center justify-center my-4">
+          <span className="text-white text-xs mb-1">click to view</span>
+          <FontAwesomeIcon
+            icon={faAnglesDown}
+            className="text-amber-400 text-sm sm:text-2xl animate-bounce"
+          />
+        </div>
+        <div className="grid grid-cols-4 gap-4 h-full w-full">
+          {projectsData.map((project, i) => (
+            <div
+              key={i}
+              className={`${boxStyle} ${
+                i === 0 || i === 4 || i === 5 || i === 6
+                  ? "col-span-2"
+                  : i === 2
+                  ? "row-span-2"
+                  : ""
+              }`}
+            >
+              <Link href={`/singleproject/${project.id}`}>
+                <img
+                  src={project.image}
+                  alt={project.alt}
+                  className="sm:h-full"
+                />
+              </Link>
+            </div>
+          ))}
+        </div>
+        <Link href="/#home">
+          <FontAwesomeIcon
+            icon={faArrowLeft}
+            className="text-amber-400 text-sm py-10 hover:text-white h-5 sm:h-10 transition-colors duration-300"
+          />
+        </Link>
       </div>
-      <Link href="/#home">
-        <FontAwesomeIcon
-          icon={faArrowLeft}
-          className="text-amber-400 text-sm my-10 hover:text-white h-5 sm:h-10 transition-colors duration-300"
-        />
-      </Link>
     </div>
   );
 };
--- src/app/components/SingleProject.jsx
+++ src/app/components/SingleProject.jsx
@@ -9,51 +9,53 @@ const SingleProject = ({ project }) => {
   }
 
   return (
-    <div className="xl:gap-16 sm:py-16 xl:px-16 my-10 mx-2 mb-20 sm:mb-1">
-      <h1 className="ml-2 mb-10 flex-grow text-center text-transparent bg-clip-text bg-gradient-to-r from-amber-100 from-20% via-amber-300 via-40% to-amber-600 to-60% text-lg sm:text-2xl lg:leading-normal font-black">
-        BKM-CODE PORTFOLIO
-      </h1>
-      <div className="grid grid-flow-row gap-8">
-        <h1 className="text-white text-xl sm:text-xl lg:text-4xl lg:leading-normal mb-2">
-          Project {project.id} : {project.title}
+    <div className="bg-[#0d0c0d]">
+      <div className="xl:gap-16 sm:py-16 xl:px-16 py-10 px-4 pb-20 sm:pb-1">
+        <h1 className="ml-2 pb-10 flex-grow text-center text-transparent bg-clip-text bg-gradient-to-r from-amber-100 from-20% via-amber-300 via-40% to-amber-600 to-60% text-lg sm:text-2xl lg:leading-normal font-black">
+          BKM-CODE PORTFOLIO
         </h1>
-        <div
-          className="h-80 md:h-96 rounded-xl relative group"
-          style={{
-            background: `url(${project.image})`,
-            backgroundSize: "cover",
-          }}
-        ></div>
-        <div className="flex flex-col gap-10 sm:w-[750px] sm:ml-60">
-          <p className="text-white mt-4">{project.description2}</p>
-          <div className="flex flex-row gap-4">
-            <Link
-              href={project.previewUrl}
-              target="_blank"
-              className="mt-1 px-1 inline-block py-1 rounded-full bg-gradient-to-br from-amber-200 to-amber-600 hover:bg-amber-500 text-amber-400"
-            >
-              <span className="block bg-[#241c0a] hover:tracking-widest transition-all ease-in-out duration-1000 rounded-full px-4 py-1 -mx-0.5 -my-0.5">
-                Go to {project.title}
-              </span>
-            </Link>
-            <Link
-              href={project.gitUrl}
-              target="_blank"
-              className="mt-1 px-1 inline-block py-1 rounded-full bg-gradient-to-br from-amber-200 to-amber-600 hover:bg-amber-500 text-amber-400"
-            >
-              <span className="block bg-[#241c0a] hover:tracking-widest transition-all ease-in-out duration-1000 rounded-full px-4 py-1 -mx-0.5 -my-0.5">
-                View Code
-              </span>
-            </Link>
+        <div className="grid grid-flow-row gap-8">
+          <h1 className="text-white text-xl sm:text-xl lg:text-4xl lg:leading-normal mb-2">
+            Project {project.id} : {project.title}
+          </h1>
+          <div
+            className="h-80 md:h-96 rounded-xl relative group"
+            style={{
+              background: `url(${project.image})`,
+              backgroundSize: "cover",
+            }}
+          ></div>
+          <div className="flex flex-col gap-10 sm:w-[750px] sm:ml-60">
+            <p className="text-white mt-4">{project.description2}</p>
+            <div className="flex flex-row gap-4">
+              <Link
+                href={project.previewUrl}
+                target="_blank"
+                className="mt-1 px-1 inline-block py-1 rounded-full bg-gradient-to-br from-amber-200 to-amber-600 hover:bg-amber-500 text-amber-400"
+              >
+                <span className="block bg-[#241c0a] hover:tracking-widest transition-all ease-in-out duration-1000 rounded-full px-4 py-1 -mx-0.5 -my-0.5">
+                  Go to {project.title}
+                </span>
+              </Link>
+              <Link
+                href={project.gitUrl}
+                target="_blank"
+                className="mt-1 px-1 inline-block py-1 rounded-full bg-gradient-to-br from-amber-200 to-amber-600 hover:bg-amber-500 text-amber-400"
+              >
+                <span className="block bg-[#241c0a] hover:tracking-widest transition-all ease-in-out duration-1000 rounded-full px-4 py-1 -mx-0.5 -my-0.5">
+                  View Code
+                </span>
+              </Link>
+            </div>
           </div>
         </div>
+        <Link href="/allprojects">
+          <FontAwesomeIcon
+            icon={faArrowLeft}
+            className="text-amber-400 text-sm py-10 hover:text-white h-5 sm:h-10 transition-colors duration-300"
+          />
+        </Link>
       </div>
-      <Link href="/allprojects">
-        <FontAwesomeIcon
-          icon={faArrowLeft}
-          className="text-amber-400 text-sm my-10 hover:text-white h-5 sm:h-10 transition-colors duration-300"
-        />
-      </Link>
     </div>
   );
 };
