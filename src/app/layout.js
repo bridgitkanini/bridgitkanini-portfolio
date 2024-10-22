ab49fa4bceeaadebec14bc6acda656c65f0eafea
bridgitkanini <bridgit.k.mutinda@gmail.com>
2024-10-22 03:11:02 +0300

    Add vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
